33171c6d809e385d531ca5d023ad2913fa836652
MWPX777 <mwpx777@gmail.com>
2021-05-12 10:52:33 -0500

    updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 
-const store = createStore(
-  allReducers,
-  // need this to use redux dev tools in Chrome
-  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
 
 
 
 ReactDOM.render(
-  <Provider store={store}>
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  </Provider>
-  ,document.getElementById('root')
+  
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
