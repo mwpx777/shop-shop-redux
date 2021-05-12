85da319612542e594187dd67d9ae5d455b6cde93
MWPX777 <mwpx777@gmail.com>
2021-05-12 10:50:39 -0500

    saving develop

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
-import allReducers from './reducers';
 import {Provider} from 'react-redux';
 
 const store = createStore(
