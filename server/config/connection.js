cfd0981c653b1c1fbd695a673e88afaa80c33b8d
MWPX777 <mwpx777@gmail.com>
2021-05-13 09:23:20 -0500

    server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
+}). catch(error => console.log(error));
+
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// db.once('open', () => {
-//   console.log('connection')
+db.once('open', () => {
+  console.log('connection')
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-// });
+});
